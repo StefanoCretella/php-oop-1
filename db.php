628689cffa2a274d8bda969f26553e942f23dc9d
StefanoCretella <cretellastefano213@gmail.com>
2024-06-24 16:32:21 +0200

    [UPD] Added cover images, improved movie details layout, optimized movie list and page design.

--- db.php
+++ db.php
@@ -6,7 +6,6 @@ $movies = [
     new Movie("Inception", "Christopher Nolan", 2010, ["Sci-Fi", "Action"], "inception_small.jpg"),
     new Movie("The Godfather", "Francis Ford Coppola", 1972, ["Crime", "Drama"], "godfather_small.jpg"),
     new Movie("Pulp Fiction", "Quentin Tarantino", 1994, ["Crime", "Drama"], "pulpfiction_small.jpg"),
-    // Aggiungi altri film con il nome del file dell'immagine rimpicciolita
 ];
 
 
